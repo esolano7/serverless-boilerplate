4bddcbb92c0825b1f15cca064cae35877aa0e124
esolano7 <esolano7@yahoo.com>
2021-08-05 12:40:20 -0600

    test

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 const serverless = require('serverless-http')
 const express = require('express')
 const router = require('./routes')
+const http = require('http')
 
 const app = express()
+const server = http.createServer(app)
 
 app.use('/', router)
 
 module.exports = {
   handler: serverless(app),
+  server: { app, server }
 }
